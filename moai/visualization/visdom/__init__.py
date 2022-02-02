755b4f0cd134538ef30552a9fcb2ccb9d8c81a15
Spiros Thermos <spthermo@gmail.com>
2022-02-02 12:08:15 +0200

    HanCo dataset added
    +datalaoder
    +play example added

--- moai/visualization/visdom/__init__.py
+++ moai/visualization/visdom/__init__.py
@@ -4,7 +4,7 @@ from moai.visualization.visdom.feature2d import Feature2d
 from moai.visualization.visdom.gizmo2d import Gizmo2d
 from moai.visualization.visdom.pose2d import Pose2d
 from moai.visualization.visdom.image_grid2d import Image_grid2d
-from moai.visualization.visdom.rendered_mesh import RenderedMesh
+# from moai.visualization.visdom.rendered_mesh import RenderedMesh
 from moai.visualization.visdom.vector import Vector
 
 __all__ = [
@@ -14,6 +14,8 @@ __all__ = [
     "Gizmo2d",
     "Pose2d",
     "Image_grid2d",
-    "RenderedMesh",
+    # "RenderedMesh",
     "Vector",
-]
+]    
+
+
