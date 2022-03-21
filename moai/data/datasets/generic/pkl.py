7b41ea21aabd3944c1b25073622ee7b5df6b9c2b
Nikolaos Zioulis <nzioulis@gmail.com>
2022-03-21 19:57:27 +0200

    dataset updates
    + add pkl data loader
    + rename raw to npz
    + fix Pano3D depth threshold

---
+++ moai/data/datasets/generic/pkl.py
@@ -0,0 +1,24 @@
+from moai.data.datasets.common import load_pkl_file
+
+import torch
+import glob
+import os
+import typing
+import logging
+
+__all__ = ["Pkl"]
+
+log = logging.getLogger(__name__)
+
+class Pkl(torch.utils.data.Dataset):
+    def __init__(self,
+        root:           str='',
+    ):
+        self.files = glob.glob(os.path.join(root, '*.pkl'))
+        log.info(f"Loaded {len(self)} .pkl files.")
+
+    def __len__(self) -> int:
+        return len(self.files)
+
+    def __getitem__(self, index: int) -> typing.Dict[str, torch.Tensor]:
+        return load_pkl_file(self.files[index])
