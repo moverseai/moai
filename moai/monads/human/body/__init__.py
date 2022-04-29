34fb30bd5b5fe875ed7f51e9190223ddd6a05e99
Nikolaos Zioulis <nzioulis@gmail.com>
2022-04-29 11:27:41 +0300

    engine modules update

--- moai/monads/human/body/__init__.py
+++ moai/monads/human/body/__init__.py
@@ -1,13 +1,9 @@
-from moai.monads.human.body.smplx import SMPLX
 from moai.monads.human.body.init_translation import InitTranslation
-from moai.monads.human.body.smpl import SMPL
 from moai.monads.human.body.joint_regressor import JointRegressor
 from moai.monads.human.body.transfer import BodyTransfer
 
 __all__ = [
-    'SMPLX',
     'InitTranslation',
-    'SMPL',
     'JointRegressor',
     'BodyTransfer',
 ]
---
+++ moai/monads/human/body/smpl/__init__.py
@@ -0,0 +1,7 @@
+from moai.monads.human.body.smpl.smpl import SMPL
+from moai.monads.human.body.smpl.smplx import SMPLX
+
+__all__ = [
+    'SMPL',
+    'SMPLX',
+]
