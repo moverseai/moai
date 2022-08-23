2d7d0bf8a3c11806566039a45866b893a36eeea7
Georgios Albanis <giorgos_al10@hotmail.com>
2022-08-23 18:26:38 +0300

    update amass loader

--- moai/data/datasets/human/amass.py
+++ moai/data/datasets/human/amass.py
@@ -21,6 +21,7 @@ class AMASS(torch.utils.data.Dataset):
     def __init__(self,
         data_root:          str,
         smplx_root:         str='',
+        device:            list=[-1],
         parts:              typing.Union[typing.List[str], str]='**',
         downsample_factor:  int=1,      
     ) -> None:
@@ -34,6 +35,7 @@ class AMASS(torch.utils.data.Dataset):
         self.sampling = downsample_factor
         frame_counter = 0
         self.subjects = { }
+        self.device = device[0] if device[0] >= 0 else 'cpu'
         for part in parts:            
             for subject in filter(os.path.isdir, glob.glob(os.path.join(data_root, part, '**'))):
                 subject_name = os.path.basename(subject)                
@@ -45,7 +47,8 @@ class AMASS(torch.utils.data.Dataset):
                 self.subjects[f"{part}_{subject_name}"] = shape
                 for action_fn in glob.glob(os.path.join(data_root, part, subject, '*_stageii.npz')):
                     data = np.load(action_fn, allow_pickle=False)
-                    frame_counter += int(data['trans'].shape[0] / downsample_factor)
+                    #frame_counter += int(data['trans'].shape[0] / downsample_factor)
+                    frame_counter += max(1,int(data['trans'].shape[0] / downsample_factor))
                     self.items[frame_counter] = { 
                         'data': data, # action_fn, # data, 
                         'gender': gender,
@@ -55,23 +58,24 @@ class AMASS(torch.utils.data.Dataset):
         self.frame_keys = list(self.items.keys())
         self.reconstruct = False
         if smplx_root:
-            smplx_create = functools.partial(smplx.create, 
-                model_path=smplx_root, model_type='smplx',
-                num_expression_coeffs=10, create_expression=True,
-                create_jaw_pose=False, create_leye_pose=False,
-                create_reye_pose=False, use_face_contour=False,
-                batch_size=1, age='adult', create_left_hand_pose=False,
-                create_right_hand_pose=False, use_pca=False,            
-                flat_hand_mean=False, create_betas=False,
-                num_betas=16, create_global_orient=False,
-                create_body_pose=False, create_transl=False,
-            )
-            self.bodies = {
-                'female': smplx_create(gender='female'),
-                'male': smplx_create(gender='male'),
-                'neutral': smplx_create(gender='neutral'),
-            }
-            del smplx_create
+            with torch.no_grad():
+                smplx_create = functools.partial(smplx.create, 
+                    model_path=smplx_root, model_type='smplx',
+                    num_expression_coeffs=10, create_expression=True,
+                    create_jaw_pose=False, create_leye_pose=False,
+                    create_reye_pose=False, use_face_contour=False,
+                    batch_size=1, age='adult', create_left_hand_pose=False,
+                    create_right_hand_pose=False, use_pca=False,            
+                    flat_hand_mean=False, create_betas=False,
+                    num_betas=16, create_global_orient=False,
+                    create_body_pose=False, create_transl=False,
+                )
+                self.bodies = {
+                    'female': smplx_create(gender='female').to(self.device),
+                    'male': smplx_create(gender='male').to(self.device),
+                    'neutral': smplx_create(gender='neutral').to(self.device),
+                }
+                del smplx_create
             for v in self.bodies.values():
                 v.requires_grad_(False)
             self.reconstruct = True
@@ -89,6 +93,7 @@ class AMASS(torch.utils.data.Dataset):
         gender = item['gender']
         betas = self.subjects[item['subject']]
         data = item['data']
+        #frame = min(frame,(len(data['pose_body'])-1))
         # data = np.load(item['data'], allow_pickle=False)
         lhand, rhand = torch.from_numpy(data['pose_hand']).split(45, dim=1)
         leye, reye = torch.from_numpy(data['pose_eye']).split(3, dim=1)        
@@ -111,21 +116,21 @@ class AMASS(torch.utils.data.Dataset):
         }
         if self.reconstruct:
             body = self.bodies[gender].forward(
-                global_orient=out['smplx']['params']['global_orient'][np.newaxis, ...],
-                betas=out['smplx']['params']['betas'][np.newaxis, ...],
-                body_pose=out['smplx']['params']['body_pose'][np.newaxis, ...],
-                left_hand_pose=out['smplx']['params']['left_hand_pose'][np.newaxis, ...],
-                right_hand_pose=out['smplx']['params']['right_hand_pose'][np.newaxis, ...],
-                jaw_pose=out['smplx']['params']['jaw_pose'][np.newaxis, ...],
-                leye_pose=out['smplx']['params']['left_eye_pose'][np.newaxis, ...],
-                reye_pose=out['smplx']['params']['right_eye_pose'][np.newaxis, ...],
-                transl=out['smplx']['params']['transl'][np.newaxis, ...],
+                global_orient=out['smplx']['params']['global_orient'][np.newaxis, ...].to(self.device),
+                betas=out['smplx']['params']['betas'][np.newaxis, ...].to(self.device),
+                body_pose=out['smplx']['params']['body_pose'][np.newaxis, ...].to(self.device),
+                left_hand_pose=out['smplx']['params']['left_hand_pose'][np.newaxis, ...].to(self.device),
+                right_hand_pose=out['smplx']['params']['right_hand_pose'][np.newaxis, ...].to(self.device),
+                jaw_pose=out['smplx']['params']['jaw_pose'][np.newaxis, ...].to(self.device),
+                leye_pose=out['smplx']['params']['left_eye_pose'][np.newaxis, ...].to(self.device),
+                reye_pose=out['smplx']['params']['right_eye_pose'][np.newaxis, ...].to(self.device),
+                transl=out['smplx']['params']['transl'][np.newaxis, ...].to(self.device),
             )
             out['smplx'].update({
                 'mesh': {
-                    'vertices': body.vertices[0],                
-                    'faces': self.bodies[gender].faces_tensor,
+                    'vertices': body.vertices[0].cpu(),                
+                    'faces': self.bodies[gender].faces_tensor.cpu(),
                     },
-                'joints': body.joints[0,:25,:],
+                'joints': body.joints[0,:25,:].cpu(),
             })
         return out
