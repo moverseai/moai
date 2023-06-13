49bd9623f92640bff357bf9a8bbe163c3c12b175
Georgios Albanis <giorgos_al10@hotmail.com>
2023-06-13 15:39:43 +0300

    Update pkl.py
    fix formatting

--- moai/data/datasets/generic/pkl.py
+++ moai/data/datasets/generic/pkl.py
@@ -11,17 +11,20 @@ __all__ = ["Pkl"]
 
 log = logging.getLogger(__name__)
 
+
 class Pkl(torch.utils.data.Dataset):
-    def __init__(self,
-        root:           str='',
+    def __init__(
+        self,
+        root: str = "",
     ):
-        self.files = glob.glob(os.path.join(root, '*.pkl'))
+        self.files = glob.glob(os.path.join(root, "*.pkl"))
         log.info(f"Loaded {len(self)} .pkl files.")
 
     def __len__(self) -> int:
         return len(self.files)
 
     def __getitem__(self, index: int) -> typing.Dict[str, torch.Tensor]:
-        return toolz.valmap(lambda t: t.detach() if isinstance(t, torch.Tensor) else t,
-            load_pkl_file(self.files[index])
+        return toolz.valmap(
+            lambda t: t.detach() if isinstance(t, torch.Tensor) else t,
+            load_pkl_file(self.files[index]),
         )
