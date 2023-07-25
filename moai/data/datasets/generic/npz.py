388cc959336700995f868896d66aa8c24b5d0673
Georgios Albanis <giorgos_al10@hotmail.com>
2023-07-25 19:58:39 +0300

    Update npz.py
    add option to include an extra key

--- moai/data/datasets/generic/npz.py
+++ moai/data/datasets/generic/npz.py
@@ -14,15 +14,17 @@ log = logging.getLogger(__name__)
 class Npz(torch.utils.data.Dataset):            #TODO add the loading different npz and combining them in the same dict case
     def __init__(self,
         root:           str='',
+        extra_key:      str=None,
     ):
         self.files = glob.glob(os.path.join(root, '*.npz'))
+        self.extra_key = extra_key
         log.info(f"Loaded {len(self)} .npz files.")
 
     def __len__(self) -> int:
         return len(self.files)
 
     def __getitem__(self, index: int) -> typing.Dict[str, torch.Tensor]:
-        return load_npz_file(self.files[index])
+        return load_npz_file(self.files[index]) if self.extra_key is None else {self.extra_key: load_npz_file(self.files[index])}
 
 class StandaloneNpz(torch.utils.data.Dataset):            #TODO add the loading different npz and combining them in the same dict case
     def __init__(self,
