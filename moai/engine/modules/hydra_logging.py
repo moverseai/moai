a8afb28c4afa6335e205137283f47608c50dc4b2
Georgios Albanis <giorgos_al10@hotmail.com>
2024-01-11 11:43:50 +0200

    Update hydra_logging.py

--- moai/engine/modules/hydra_logging.py
+++ moai/engine/modules/hydra_logging.py
@@ -1,6 +1,5 @@
 # instantiate hydra logging for torchserve
 import omegaconf.omegaconf
-import typing
 import logging
 
 
@@ -13,7 +12,5 @@ class HydraLogging(object):
     ):
         # NOTE: this is a workaround for the fact that torchserve
         # does not support hydra logging
-        #
-        print(f"Hydra logging: {job}")
         # Applying the logging configuration
         logging.config.dictConfig(omegaconf.OmegaConf.to_container(job, resolve=True))
