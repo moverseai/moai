061e30c24d56cf790246621b68890ab696cef8fd
nmvrs <nick@moverse.ai>
2024-02-14 17:25:39 +0200

    Update skeleton.py

--- moai/nn/convolution/skeleton/skeleton.py
+++ moai/nn/convolution/skeleton/skeleton.py
@@ -20,10 +20,10 @@ class SkeletonConvolution(torch.nn.Module):
         bias:           bool=True,
         padding:        int=0,
         padding_mode:   str='zeros',
-        neighbour_list: typing.Sequence[typing.Sequence[int]]=[],
+        neighbors: typing.Sequence[typing.Sequence[int]]=[],
     ):
         super().__init__()
-        self.joint_num = len(neighbour_list)
+        self.joint_num = len(neighbors)
         if in_channels % self.joint_num != 0 or out_channels % self.joint_num != 0:
             log.error("[SkeletonConvolution] Input and output channels should be a multiple of the number of joints.")            
         self.in_channels_per_joint = in_channels // self.joint_num
@@ -31,9 +31,9 @@ class SkeletonConvolution(torch.nn.Module):
         if padding_mode == 'zeros': 
             padding_mode = 'constant'
 
-        self.expanded_neighbour_list = []
-        self.expanded_neighbour_list_offset = []
-        self.neighbour_list = neighbour_list
+        self.expanded_neighbors = []
+        self.expanded_neighbors_offset = []
+        self.neighbors = neighbors
         
         self.kernel_size = kernel_size
         self.stride = stride
@@ -43,12 +43,12 @@ class SkeletonConvolution(torch.nn.Module):
         self.padding_mode = padding_mode
         self._padding_repeated_twice = (padding, padding)
 
-        for neighbour in neighbour_list:
+        for neighbour in neighbors:
             expanded = []
             for k in neighbour:
                 for i in range(self.in_channels_per_joint):
                     expanded.append(k * self.in_channels_per_joint + i)
-            self.expanded_neighbour_list.append(expanded)
+            self.expanded_neighbors.append(expanded)
         
         self.weight = torch.zeros(out_channels, in_channels, kernel_size)
         if bias:
@@ -57,14 +57,14 @@ class SkeletonConvolution(torch.nn.Module):
             self.register_parameter('bias', None)
 
         self.mask = torch.zeros_like(self.weight)
-        for i, neighbour in enumerate(self.expanded_neighbour_list):
+        for i, neighbour in enumerate(self.expanded_neighbors):
             self.mask[self.out_channels_per_joint * i: self.out_channels_per_joint * (i + 1), neighbour, ...] = 1
         self.mask = torch.nn.parameter.Parameter(self.mask, requires_grad=False) #TODO: why param?
 
         self.reset_parameters()
 
     def reset_parameters(self):
-        for i, neighbour in enumerate(self.expanded_neighbour_list):
+        for i, neighbour in enumerate(self.expanded_neighbors):
             #NOTE: Use temporary variable to avoid assign to copy of slice, which might lead to un expected result
             tmp = torch.zeros_like(self.weight[self.out_channels_per_joint * i: self.out_channels_per_joint * (i + 1),
                                    neighbour, ...])
