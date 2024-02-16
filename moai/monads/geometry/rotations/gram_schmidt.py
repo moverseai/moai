3c1981938ac5ceb9d391e7073b320696c4fefd23
Spyridon Thermos <spthermo@gmail.com>
2024-02-16 20:33:50 +0200

    updated gram-schmidt implementation to handle >4d tensors

--- moai/monads/geometry/rotations/gram_schmidt.py
+++ moai/monads/geometry/rotations/gram_schmidt.py
@@ -16,6 +16,8 @@ class GramSchmidt(torch.nn.Module):
         super().__init__()
 
     def forward(self, 
-        sixd: torch.Tensor, # [B, R, 3, 2]
-    ) -> torch.Tensor: # [B, R, 3, 3]
-        return roma.special_gramschmidt(sixd.view(sixd.shape[0], -1, 3, 2))
+        sixd: torch.Tensor, # [..., R, 3, 2]
+    ) -> torch.Tensor: # [..., R, 3, 3]
+        out_shape = list(sixd.shape)
+        out_shape[-1] = 3
+        return roma.special_gramschmidt(sixd.view(-1, sixd.shape[-3], 3, 2)).view(out_shape)
