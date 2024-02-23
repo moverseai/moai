26b207c3f8f54dd66534121f6c4a6b4cf1e96608
Georgios Albanis <giorgos_al10@hotmail.com>
2024-02-23 18:16:40 +0200

    add latent distributions

---
+++ moai/modules/lightning/distribution/__init__.py
@@ -0,0 +1,7 @@
+from moai.modules.lightning.distribution.homeomorphic import Homeomorphic
+from moai.modules.lightning.distribution.basic import Basic
+
+__all__ = [
+    "Homeomorphic",
+    "Basic",
+]
