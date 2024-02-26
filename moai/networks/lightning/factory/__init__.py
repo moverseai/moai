f68227437d4a9c3d7e602cd1736fbf51b89954e1
Georgios Albanis <giorgos_al10@hotmail.com>
2024-02-26 13:04:42 +0200

    Update __init__.py
    add onnx dependency import guard

--- moai/networks/lightning/factory/__init__.py
+++ moai/networks/lightning/factory/__init__.py
@@ -5,7 +5,10 @@ from moai.networks.lightning.factory.multibranch import MultiBranch
 from moai.networks.lightning.factory.hrnet import HRNet
 from moai.networks.lightning.factory.cascade import Cascade
 from moai.networks.lightning.factory.unet import UNet
-from moai.networks.lightning.factory.onnx import Onnx
+try:
+    from moai.networks.lightning.factory.onnx import Onnx
+except ImportError:
+    print('ONNX is not available, please make sure you have installed all the dependencies (e.g. onnx, onnx-runtime).')
 from moai.networks.lightning.factory.gan import GenerativeAdversarialNetwork
 from moai.networks.lightning.factory.gan import NullGenerativeAdversarialNetwork
 
