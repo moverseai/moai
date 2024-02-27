8a2407abb80b73148a647fcb311966bc2d491403
Georgios Albanis <giorgos_al10@hotmail.com>
2024-02-27 15:28:44 +0200

    support for internal precision of float32 matrix multiplications.
    - related to Pytorch 2.2
    - check: https://pytorch.org/docs/stable/generated/torch.set_float32_matmul_precision.html

---
+++ moai/engine/modules/precision.py
@@ -0,0 +1,23 @@
+import torch
+import logging
+
+log = logging.getLogger(__name__)
+
+# NOTE: see https://github.com/Lightning-AI/pytorch-lightning/discussions/16698
+# NOTE: see https://pytorch.org/docs/stable/generated/torch.set_float32_matmul_precision.html
+
+__all__ = ["Precision"]
+
+
+class Precision(object):
+    def __init__(
+        self,
+        precision: str = "highest",  # one of 'highest', 'high', 'medium'
+    ):
+        # Supports three settings:
+        #  - highest: float32 matrix multiplications use the float32 datatype (24 mantissa bits)
+        #  - high: float32 matrix multiplications either use the TensorFloat32 datatype (10 mantissa bits) or the float32 datatype
+        #  - medium: medium: float32 matrix multiplications use the TensorFloat32 datatype (8 mantissa bits)
+
+        log.info(f"Setting PyTorch precision to {precision}.")
+        torch.set_float32_matmul_precision(precision)
