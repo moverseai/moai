84dfdd724d639185edba66d8964a41e061cdb85d
Georgios Albanis <giorgos_al10@hotmail.com>
2024-05-25 01:36:02 +0300

    feat: add action to config & change match statemements
     - add action to __moai__ config
    - change match to else-if statements to support Python < 3.10
    - remove unused keys from initializers

--- moai/action/run.py
+++ moai/action/run.py
@@ -15,44 +15,96 @@ log = logging.getLogger(__name__)
 
 def _specialize_config(cfg: DictConfig) -> None:
     #TODO: remove unused keys for metrics etc. inside the named collections/flows
-    match cfg.action:
-        case 'fit':
-            if OmegaConf.select(cfg, f'{Constants._MOAI_EXECUTION_}.test'):
-                del OmegaConf.select(cfg, f'{Constants._MOAI_EXECUTION_}').test
-            if OmegaConf.select(cfg, f'{Constants._MOAI_EXECUTION_}.predict'):
-                del OmegaConf.select(cfg, f'{Constants._MOAI_EXECUTION_}').predict
-            if OmegaConf.select(cfg, f'{Constants._MOAI_MONITORING_}.test'):
-                del OmegaConf.select(cfg, f'{Constants._MOAI_MONITORING_}').test
-            if OmegaConf.select(cfg, f'{Constants._MOAI_MONITORING_}.predict'):
-                del OmegaConf.select(cfg, f'{Constants._MOAI_MONITORING_}').predict                
-        case 'test':
-            if OmegaConf.select(cfg, f'{Constants._MOAI_EXECUTION_}.fit'):
-                del OmegaConf.select(cfg, f'{Constants._MOAI_EXECUTION_}').fit
-            if OmegaConf.select(cfg, f'{Constants._MOAI_EXECUTION_}.val'):
-                del OmegaConf.select(cfg, f'{Constants._MOAI_EXECUTION_}').val
-            if OmegaConf.select(cfg, f'{Constants._MOAI_EXECUTION_}.predict'):
-                del OmegaConf.select(cfg, f'{Constants._MOAI_EXECUTION_}').predict
-            if OmegaConf.select(cfg, f'{Constants._MOAI_MONITORING_}.fit'):
-                del OmegaConf.select(cfg, f'{Constants._MOAI_MONITORING_}').fit
-            if OmegaConf.select(cfg, f'{Constants._MOAI_MONITORING_}.val'):
-                del OmegaConf.select(cfg, f'{Constants._MOAI_MONITORING_}').val
-            if OmegaConf.select(cfg, f'{Constants._MOAI_MONITORING_}.predict'):
-                del OmegaConf.select(cfg, f'{Constants._MOAI_MONITORING_}').predict
-        case 'predict':
-            if OmegaConf.select(cfg, f'{Constants._MOAI_EXECUTION_}.fit'):
-                del OmegaConf.select(cfg, f'{Constants._MOAI_EXECUTION_}').fit
-            if OmegaConf.select(cfg, f'{Constants._MOAI_EXECUTION_}.val'):
-                del OmegaConf.select(cfg, f'{Constants._MOAI_EXECUTION_}').val
-            if OmegaConf.select(cfg, f'{Constants._MOAI_EXECUTION_}.test'):
-                del OmegaConf.select(cfg, f'{Constants._MOAI_EXECUTION_}').test
-            if OmegaConf.select(cfg, f'{Constants._MOAI_MONITORING_}.fit'):
-                del OmegaConf.select(cfg, f'{Constants._MOAI_MONITORING_}').fit
-            if OmegaConf.select(cfg, f'{Constants._MOAI_MONITORING_}.val'):
-                del OmegaConf.select(cfg, f'{Constants._MOAI_MONITORING_}').val
-            if OmegaConf.select(cfg, f'{Constants._MOAI_MONITORING_}.test'):
-                del OmegaConf.select(cfg, f'{Constants._MOAI_MONITORING_}').test
-        case _:
-            log.error(f"Erroneous `action` [{cfg.action}] specified on `run` mode.")
+    # match cfg.action:
+    #     case 'fit':
+    #         if OmegaConf.select(cfg, f'{Constants._MOAI_EXECUTION_}.test'):
+    #             del OmegaConf.select(cfg, f'{Constants._MOAI_EXECUTION_}').test
+    #         if OmegaConf.select(cfg, f'{Constants._MOAI_EXECUTION_}.predict'):
+    #             del OmegaConf.select(cfg, f'{Constants._MOAI_EXECUTION_}').predict
+    #         if OmegaConf.select(cfg, f'{Constants._MOAI_MONITORING_}.test'):
+    #             del OmegaConf.select(cfg, f'{Constants._MOAI_MONITORING_}').test
+    #         if OmegaConf.select(cfg, f'{Constants._MOAI_MONITORING_}.predict'):
+    #             del OmegaConf.select(cfg, f'{Constants._MOAI_MONITORING_}').predict
+    #     case 'test':
+    #         if OmegaConf.select(cfg, f'{Constants._MOAI_EXECUTION_}.fit'):
+    #             del OmegaConf.select(cfg, f'{Constants._MOAI_EXECUTION_}').fit
+    #         if OmegaConf.select(cfg, f'{Constants._MOAI_EXECUTION_}.val'):
+    #             del OmegaConf.select(cfg, f'{Constants._MOAI_EXECUTION_}').val
+    #         if OmegaConf.select(cfg, f'{Constants._MOAI_EXECUTION_}.predict'):
+    #             del OmegaConf.select(cfg, f'{Constants._MOAI_EXECUTION_}').predict
+    #         if OmegaConf.select(cfg, f'{Constants._MOAI_MONITORING_}.fit'):
+    #             del OmegaConf.select(cfg, f'{Constants._MOAI_MONITORING_}').fit
+    #         if OmegaConf.select(cfg, f'{Constants._MOAI_MONITORING_}.val'):
+    #             del OmegaConf.select(cfg, f'{Constants._MOAI_MONITORING_}').val
+    #         if OmegaConf.select(cfg, f'{Constants._MOAI_MONITORING_}.predict'):
+    #             del OmegaConf.select(cfg, f'{Constants._MOAI_MONITORING_}').predict
+    #     case 'predict':
+    #         if OmegaConf.select(cfg, f'{Constants._MOAI_EXECUTION_}.fit'):
+    #             del OmegaConf.select(cfg, f'{Constants._MOAI_EXECUTION_}').fit
+    #         if OmegaConf.select(cfg, f'{Constants._MOAI_EXECUTION_}.val'):
+    #             del OmegaConf.select(cfg, f'{Constants._MOAI_EXECUTION_}').val
+    #         if OmegaConf.select(cfg, f'{Constants._MOAI_EXECUTION_}.test'):
+    #             del OmegaConf.select(cfg, f'{Constants._MOAI_EXECUTION_}').test
+    #         if OmegaConf.select(cfg, f'{Constants._MOAI_MONITORING_}.fit'):
+    #             del OmegaConf.select(cfg, f'{Constants._MOAI_MONITORING_}').fit
+    #         if OmegaConf.select(cfg, f'{Constants._MOAI_MONITORING_}.val'):
+    #             del OmegaConf.select(cfg, f'{Constants._MOAI_MONITORING_}').val
+    #         if OmegaConf.select(cfg, f'{Constants._MOAI_MONITORING_}.test'):
+    #             del OmegaConf.select(cfg, f'{Constants._MOAI_MONITORING_}').test
+    #     case _:
+    #         log.error(f"Erroneous `action` [{cfg.action}] specified on `run` mode.")
+    if cfg.action == 'fit':
+        if OmegaConf.select(cfg, f'{Constants._MOAI_EXECUTION_}.test'):
+            del OmegaConf.select(cfg, f'{Constants._MOAI_EXECUTION_}').test
+        if OmegaConf.select(cfg, f'{Constants._MOAI_EXECUTION_}.predict'):
+            del OmegaConf.select(cfg, f'{Constants._MOAI_EXECUTION_}').predict
+        if OmegaConf.select(cfg, f'{Constants._MOAI_MONITORING_}.test'):
+            del OmegaConf.select(cfg, f'{Constants._MOAI_MONITORING_}').test
+        if OmegaConf.select(cfg, f'{Constants._MOAI_MONITORING_}.predict'):
+            del OmegaConf.select(cfg, f'{Constants._MOAI_MONITORING_}').predict
+        if OmegaConf.select(cfg, f'{Constants._MOAI_INITIALIZE_}.test'):
+            del OmegaConf.select(cfg, f'{Constants._MOAI_INITIALIZE_}').test
+        if OmegaConf.select(cfg, f'{Constants._MOAI_INITIALIZE_}.predict'):
+            del OmegaConf.select(cfg, f'{Constants._MOAI_INITIALIZE_}').predict
+        # if OmegaConf.select(cfg, f'{Constants._MOAI_INITIALIZE_}.fit'):
+        #     cfg._moai_._init_ = OmegaConf.select(cfg, f'{Constants._MOAI_INITIALIZE_}.fit')
+
+    elif cfg.action == 'test':
+        if OmegaConf.select(cfg, f'{Constants._MOAI_EXECUTION_}.fit'):
+            del OmegaConf.select(cfg, f'{Constants._MOAI_EXECUTION_}').fit
+        if OmegaConf.select(cfg, f'{Constants._MOAI_EXECUTION_}.val'):
+            del OmegaConf.select(cfg, f'{Constants._MOAI_EXECUTION_}').val
+        if OmegaConf.select(cfg, f'{Constants._MOAI_EXECUTION_}.predict'):
+            del OmegaConf.select(cfg, f'{Constants._MOAI_EXECUTION_}').predict
+        if OmegaConf.select(cfg, f'{Constants._MOAI_MONITORING_}.fit'):
+            del OmegaConf.select(cfg, f'{Constants._MOAI_MONITORING_}').fit
+        if OmegaConf.select(cfg, f'{Constants._MOAI_MONITORING_}.val'):
+            del OmegaConf.select(cfg, f'{Constants._MOAI_MONITORING_}').val
+        if OmegaConf.select(cfg, f'{Constants._MOAI_MONITORING_}.predict'):
+            del OmegaConf.select(cfg, f'{Constants._MOAI_MONITORING_}').predict
+        if OmegaConf.select(cfg, f'{Constants._MOAI_INITIALIZE_}.fit'):
+            del OmegaConf.select(cfg, f'{Constants._MOAI_INITIALIZE_}').fit
+        if OmegaConf.select(cfg, f'{Constants._MOAI_INITIALIZE_}.predict'):
+            del OmegaConf.select(cfg, f'{Constants._MOAI_INITIALIZE_}').predict
+    elif cfg.action == 'predict':
+        if OmegaConf.select(cfg, f'{Constants._MOAI_EXECUTION_}.fit'):
+            del OmegaConf.select(cfg, f'{Constants._MOAI_EXECUTION_}').fit
+        if OmegaConf.select(cfg, f'{Constants._MOAI_EXECUTION_}.val'):
+            del OmegaConf.select(cfg, f'{Constants._MOAI_EXECUTION_}').val
+        if OmegaConf.select(cfg, f'{Constants._MOAI_EXECUTION_}.test'):
+            del OmegaConf.select(cfg, f'{Constants._MOAI_EXECUTION_}').test
+        if OmegaConf.select(cfg, f'{Constants._MOAI_MONITORING_}.fit'):
+            del OmegaConf.select(cfg, f'{Constants._MOAI_MONITORING_}').fit
+        if OmegaConf.select(cfg, f'{Constants._MOAI_MONITORING_}.val'):
+            del OmegaConf.select(cfg, f'{Constants._MOAI_MONITORING_}').val
+        if OmegaConf.select(cfg, f'{Constants._MOAI_MONITORING_}.test'):
+            del OmegaConf.select(cfg, f'{Constants._MOAI_MONITORING_}').test
+        if OmegaConf.select(cfg, f'{Constants._MOAI_INITIALIZE_}.test'):
+            del OmegaConf.select(cfg, f'{Constants._MOAI_INITIALIZE_}').test
+        if OmegaConf.select(cfg, f'{Constants._MOAI_INITIALIZE_}.fit'):
+            del OmegaConf.select(cfg, f'{Constants._MOAI_INITIALIZE_}').fit
+    else:
+        log.error(f"Erroneous `action` [{cfg.action}] specified on `run` mode.")
 
 def run(cfg: DictConfig):
     _specialize_config(cfg)
@@ -63,6 +115,9 @@ def run(cfg: DictConfig):
         f.write(OmegaConf.to_yaml(cfg, resolve=False, sort_keys=False))
     # engine = hydra.utils.instantiate(cfg.engine)
     engine = Engine(cfg.engine.modules) #NOTE: ctor before model/runner
+    OmegaConf.set_struct(cfg, False)
+    cfg._moai_._action_ = cfg.action
+    OmegaConf.set_struct(cfg, True)
     model = hydra.utils.instantiate(cfg.model,
         data=cfg.data,
         # visualization=assign(cfg, "visualization"),
