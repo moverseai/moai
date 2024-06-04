019d4d5a78abeb0615a6e76626723fe0fe930bdf
Nick <nick@moverse.ai>
2024-06-04 12:38:33 +0300

    feat: add :moai: logging

---
+++ moai/logging/moai_filter.py
@@ -0,0 +1,9 @@
+import logging
+
+class MoaiFilter(logging.Filter):
+    def __init__(self) -> None:
+        super().__init__("moai")
+
+    def filter(self, record):
+        record.msg = f":moai: {record.msg}"
+        return (record)
