2c1aaee455b271328d71bed416a57ab7d7689e43
Georgios Albanis <giorgos_al10@hotmail.com>
2024-06-26 10:42:34 +0300

    wip: update optimizer

--- moai/serve/optimizer.py
+++ moai/serve/optimizer.py
@@ -4,10 +4,13 @@ import time
 import typing
 
 import benedict
+import hydra.utils as hyu
 import toolz
 import torch
+from hydra import compose, initialize
+from omegaconf.omegaconf import OmegaConf
+from ts.metrics.metric_type_enum import MetricTypes
 
-# from moai.serve.model import ModelServer
 try:
     from model import (
         ModelServer,  # get model from local directory, otherwise configs could not be loaded
@@ -23,7 +26,6 @@ from ts.handler_utils.utils import send_intermediate_predict_response
 from moai.core.execution.constants import Constants as C
 from moai.core.model import _create_assigner
 from moai.engine.callbacks.model import ModelCallbacks
-from moai.engine.runner import LightningRunner
 from moai.utils.funcs import get_list
 
 log = logging.getLogger(__name__)  # NOTE: check name when logging from serve
@@ -39,62 +41,93 @@ __all__ = ["OptimizerServer"]
 class OptimizerServer(ModelServer):
     def __init__(self) -> None:
         super().__init__()
-        self.handle = self.fitting_handle
 
-    def fitting_handle(
-        self, data: typing.Mapping[str, typing.Any], context: typing.Any
-    ):
+    def initialize(self, context):
+        # call parent class initialize
+        super().initialize(context)
+        main_conf = context.manifest["model"]["modelName"].replace("_", "/")
+        # set model to training true before calling the training step
+        self.model.train()
+        try:
+            with initialize(
+                config_path="conf/" + "/".join(main_conf.split("/")[0:-1]),
+                job_name=main_conf,
+                version_base="1.3",
+            ):
+                cfg = compose(
+                    config_name=main_conf.split("/")[-1],
+                    overrides=self._get_overrides(),
+                    return_hydra_config=True,
+                )
+                log.info("Loading trainer...")
+                OmegaConf.set_struct(cfg, False)
+                cfg.engine.runner.devices = [self.device.index]
+                # TODO: check if device is set correctly
+                OmegaConf.set_struct(cfg, True)
+                self.trainer = hyu.instantiate(
+                    cfg.engine.runner,
+                    model_callbacks=ModelCallbacks(model=self.model),
+                    _recursive_=False,
+                )
+        except Exception as e:
+            log.error(f"An error has occured while loading the trainer:\n{e}")
+
+    def handle(self, data: typing.Mapping[str, typing.Any], context: typing.Any):
         """
-        Handler responsible for returning a streaming response.
-        This handler is used when the model is in fit mode.
+        Handle function responsible for returning an intermediate response.
+        Used for fit mode.
         """
         log.info("Fitting handler called.")
+        self.optimization_step = 0
         start_time = time.time()
         output = None  # DEBUG
 
         self.context = context
-        metrics = self.context.metrics
 
         batch = self.preprocess(data)
         batch_idx = batch.get("batch_idx", 0)
-        # TODO call main inference function
-        # set model to training true before calling the training step
-        self.model.train()
-        # TODO: add post processing handler
-        # Do I need to setup a lightning trainer?
-        # TODO: device should be taken from the input request
-        trainer = LightningRunner(
-            model_callbacks=ModelCallbacks([self.model]), devices=[0]
-        )
+        self.trainer.strategy._lightning_module = (
+            self.model
+        )  # IMPORTANT: we need to set lightning module to the model
+        self.model._trainer = self.trainer
+        # self.model.configure_optimizers()
+        self.trainer.strategy._lightning_optimizers = self.model.configure_optimizers()[
+            0
+        ]
 
         # reimplement training step to get access to intermediate outputs (e.g. from intermediate iterations; this should be reported to the response)
         def closure(tensors, index, steps, stage, optimizer, objective):
-            # def backward_fn(loss: torch.Tensor, optimizer: torch.optim.Optimizer) -> None:
-            # call._call_strategy_hook(self.trainer, "backward", loss, optimizer)
             for step in steps:
                 tensors = self.model.named_flows[step](tensors)
             self.model.named_objectives[objective](tensors)
             loss = tensors[f"{C._MOAI_LOSSES_}.total"]
-            is_first_batch_to_accumulate = index % trainer.accumulate_grad_batches == 0
-            if trainer.accumulate_grad_batches == 1 or not is_first_batch_to_accumulate:
-                call._call_callback_hooks(trainer, "on_before_zero_grad", optimizer)
+            is_first_batch_to_accumulate = (
+                index % self.trainer.accumulate_grad_batches == 0
+            )
+            if (
+                self.trainer.accumulate_grad_batches == 1
+                or not is_first_batch_to_accumulate
+            ):
+                call._call_callback_hooks(
+                    self.trainer, "on_before_zero_grad", optimizer
+                )
                 call._call_lightning_module_hook(
-                    trainer, "on_before_zero_grad", optimizer
+                    self.trainer, "on_before_zero_grad", optimizer
                 )
                 call._call_lightning_module_hook(
                     self.trainer,
                     "optimizer_zero_grad",
-                    trainer.current_epoch,
+                    self.trainer.current_epoch,
                     index,
                     optimizer,
                 )
-            call._call_strategy_hook(trainer, "backward", loss, optimizer)
+            call._call_strategy_hook(self.trainer, "backward", loss, optimizer)
             self.optimization_step += 1
             if monitor := toolz.get_in(
                 [C._FIT_, C._OPTIMIZATION_STEP_, stage], self.model.monitor
             ):
                 should_monitor = (
-                    self.model.optimization_step % monitor.get("_frequency_", 1) == 0
+                    self.optimization_step % monitor.get("_frequency_", 1) == 0
                 )
                 if (
                     tensor_monitor_steps := get_list(monitor, C._MONITORS_)
@@ -143,15 +176,17 @@ class OptimizerServer(ModelServer):
             )
             for iter in range(proc.get(C._ITERATIONS_, 1)):
                 if (  # when the strategy handles accumulation, we want to always call the optimizer step
-                    not trainer.strategy.handles_gradient_accumulation
-                    and trainer.fit_loop._should_accumulate()
+                    not self.trainer.strategy.handles_gradient_accumulation
+                    and self.trainer.fit_loop._should_accumulate()
                 ):  # For gradient accumulation calculate loss (train step + train step end)
-                    with _block_parallel_sync_behavior(trainer.strategy, block=True):
+                    with _block_parallel_sync_behavior(
+                        self.trainer.strategy, block=True
+                    ):
                         current_closure()  # automatic_optimization=True: perform ddp sync only when performing optimizer_step
                 else:
                     if optimizer is not None:
                         self.model.optimizer_step(
-                            trainer.current_epoch,
+                            self.trainer.current_epoch,
                             batch_idx,
                             optimizer,
                             current_closure,
@@ -160,16 +195,20 @@ class OptimizerServer(ModelServer):
                         # key should be parseed
                         # from self.context.request_ids.keys()
                         key = 0  # DEBUG
+                        max_iter = proc.get(C._ITERATIONS_, 1) - 1
                         batch[key] = (
-                            f"Running Stage {stage} and Iteration {iter} from the model."
+                            f"Running Stage {stage} and Iteration {iter} from the model, with completion percentage {iter/max_iter}."
                         )
-                        # send_intermediate_predict_response(
-                        #   batch,
-                        #  self.context.request_ids,
-                        #  "Intermediate response from the model.",
-                        #  200,
-                        #  self.context,
-                        # )
+                        print(batch[key])
+                        log.info(batch[key])
+                        log.info("request_ids: ", self.context.request_ids)
+                        send_intermediate_predict_response(
+                            batch,
+                            self.context.request_ids,
+                            "Intermediate response from the model.",
+                            200,
+                            self.context,
+                        )
                     else:  # NOTE: w/o an optim, it is a tensor setup step (e.g. inference)
                         with torch.no_grad():
                             for flow in flows:
@@ -181,53 +220,55 @@ class OptimizerServer(ModelServer):
                                 batch[key] = (
                                     f"Running Stage {stage} and Iteration {iter} from the model."
                                 )
-                                # send_intermediate_predict_response(
-                                #  batch,
-                                # self.context.request_ids,
-                                # "Intermediate response from the model.",
-                                # 200,
-                                # self.context,
-                                # )
+                                send_intermediate_predict_response(
+                                    batch,
+                                    self.context.request_ids,
+                                    "Intermediate response from the model.",
+                                    200,
+                                    self.context,
+                                )
                                 # TODO: this should be returned as intermediate response in the HTTP response
-                # TODO: do we need to add monitoring here?
-                # We should add monitoring here and use the contect metrics to report them in GET /metrics
-                # if iter_monitor_stage := toolz.get_in(
-                #     [C._FIT_, C._STAGES_, stage], self.monitor
-                # ):
-                #     frequency = toolz.get(C._FREQUENCY_, iter_monitor_stage, 1)
-                #     should_monitor = iter % frequency == 0
-                #     if (
-                #         iter_tensor_monitor := iter_monitor_stage.get(
-                #             C._MONITORS_, None
-                #         )
-                #     ) and should_monitor:
-                #         for step in (
-                #             toolz.get(C._FLOWS_, iter_monitor_stage, None) or []
-                #         ):
-                #             self.model.named_flows[step](batch)
-                #         for metric in (
-                #             toolz.get(C._METRICS_, iter_monitor_stage, None) or []
-                #         ):
-                #             self.model.named_metrics[metric](batch)
-                #         extras = {  # TODO: step => 'lightning_step'
-                #             "lightning_step": self.model.global_step,
-                #             "epoch": self.model.current_epoch,
-                #             "batch_idx": batch_idx,
-                #             "stage": stage,
-                #             "iteration": iter,
-                #         }
-                #         for step in iter_tensor_monitor:
-                #             self.model.named_monitors[step](batch, extras)
-                #         should_stop = False
-                #         for criterion in get_list(iter_monitor_stage, C._TERMINATION_):
-                #             if self.model.named_criteria[criterion](batch, extras):
-                #                 should_stop = True
-                #                 break
-                #         if should_stop:
-                #             log.info(
-                #                 f"Terminating {stage} @ {iter} with criterion [{criterion}] !"
-                #             )
-                #             break
+                if iter_monitor_stage := toolz.get_in(
+                    [C._FIT_, C._STAGES_, stage], self.model.monitor
+                ):
+                    frequency = toolz.get(C._FREQUENCY_, iter_monitor_stage, 1)
+                    should_monitor = iter % frequency == 0
+                    # calculate metrics seperately
+                    if (
+                        iter_tensor_metric := iter_monitor_stage.get(C._METRICS_, None)
+                    ) and should_monitor:
+                        for metric in (
+                            toolz.get(C._METRICS_, iter_monitor_stage, None) or []
+                        ):
+                            self.model.named_metrics[metric](batch)
+                    if (
+                        iter_tensor_monitor := iter_monitor_stage.get(
+                            C._MONITORS_, None
+                        )
+                    ) and should_monitor:
+                        for step in (
+                            toolz.get(C._FLOWS_, iter_monitor_stage, None) or []
+                        ):
+                            self.model.named_flows[step](batch)
+                        extras = {  # TODO: step => 'lightning_step'
+                            "lightning_step": self.model.global_step,
+                            "epoch": self.model.current_epoch,
+                            "batch_idx": batch_idx,
+                            "stage": stage,
+                            "iteration": iter,
+                        }
+                        for step in iter_tensor_monitor:
+                            self.model.named_monitors[step](batch, extras)
+                        should_stop = False
+                        for criterion in get_list(iter_monitor_stage, C._TERMINATION_):
+                            if self.model.named_criteria[criterion](batch, extras):
+                                should_stop = True
+                                break
+                        if should_stop:
+                            log.info(
+                                f"Terminating {stage} @ {iter} with criterion [{criterion}] !"
+                            )
+                            break
             # call the copy params for initialization
             if assign_params is not None:
                 frequency = assign_params.get(
@@ -251,78 +292,29 @@ class OptimizerServer(ModelServer):
 
         # TODO: add post processing handler
         stop_time = time.time()
-        metrics.add_time(
+
+        # TODO: add post processing handler
+        stop_time = time.time()
+        self.context.metrics.add_time(
             "FittingHandlerTime", round((stop_time - start_time) * 1000, 2), None, "ms"
         )
+        # NOTE: Do we need to call on batch end to calculate metrics?
+        # self.model.on_train_batch_end(outputs, batch, batch_idx)
+        # report metrics
+        for key, val in batch[C._MOAI_METRICS_].items():
+            self.context.metrics.add_metric(
+                name=key,
+                value=float(val.detach().cpu().numpy()),
+                unit="value",
+                metric_type=MetricTypes.GAUGE,
+            )
+        # report losses
+        for key, val in batch[C._MOAI_LOSSES_]["raw"].items():
+            self.context.metrics.add_metric(
+                name=key,
+                value=float(val.detach().cpu().numpy()),
+                unit="value",
+                metric_type=MetricTypes.GAUGE,
+            )
+        # TODO: call post processing handler
         return output
-
-
-if __name__ == "__main__":
-    from requests.structures import CaseInsensitiveDict
-    from ts.context import Context, RequestProcessor
-
-    manifest = {
-        "createdOn": "06/06/2021 19:42:51",
-        "runtime": "python",
-        "model": {
-            "modelName": "projects_shape_fit",
-            "handler": "model.py",
-            "modelVersion": "1.0",
-        },
-        "archiverVersion": "0.5.3",
-    }
-    # intialize model
-    context = Context(
-        model_name="projects_shape_fit",
-        model_dir="./",
-        manifest=manifest,
-        batch_size=1,
-        gpu=0,
-        mms_version="",
-    )
-    import os
-
-    # Set environment variables
-    os.environ["HYDRA_FULL_ERROR"] = "1"
-    os.environ["MOVERSE_SMPL_MODEL"] = (
-        "C:/Users/giorg/Documents/Projects/markerless-mocap/third_party/smplx/transfer_data/body_models/smpl/basicmodel_neutral_lbs_10_207_0_v1.1.0.pkl"
-    )
-    os.environ["MOVERSE_BDTK_ROOT"] = "D:/repos/bdtk"
-    os.environ["MOVERSE_GENDER"] = "neutral"
-    os.environ["MOVERSE_SMPL_ROOT"] = (
-        "C:/Users/giorg/Documents/Projects/markerless-mocap/third_party/smplx/transfer_data/body_models"
-    )
-    os.environ["MOVERSE_BETAS"] = "10"
-    os.environ["MOVERSE_WINDOW_SIZE"] = "10"
-    os.environ["MOVERSE_STRIDE"] = "9"
-    os.environ["MOVERSE_POSE_W"] = "10.78"
-    os.environ["MOVERSE_SHAPE_W"] = "50.0"
-    os.environ["MOVERSE_LANDMARKS_W"] = "1"
-    os.environ["MOVERSE_PRIOR_ROOT"] = (
-        "C:/Users/giorg/Documents/Projects/markerbased-mocap/actions/train/2024-04-12/00-14-20-svae/svae/version_0/checkpoints/epoch_5.ckpt"
-    )
-    os.environ["MOVERSE_PQ_ROOT"] = (
-        "D:/repos/dot/actions/fit/2024-06-18/11-20-16-bundle/bundle.pq"
-    )
-    headers = CaseInsensitiveDict()
-    headers["Content-Type"] = "application/json"
-
-    rp = RequestProcessor(headers)
-    # context.request_processor = [rp]
-    context.request_processor = {
-        "idx": rp,
-    }
-
-    model = ModelServer()
-    context.request_ids = {
-        "idx": "1fsdf",
-    }
-    model.initialize(context)
-    model.handle(
-        {
-            "vrs": "//NAS5DD7C0/mov-dev/mvrs_files/lite-full-heavy-recordings/5dc19982-a7b2-4955-ad93-e162b992f22e/pushups-low-lite.mvrs",
-            "body": "test",
-        },
-        context,
-    )
-    # model.handle()
